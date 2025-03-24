be4f4252cfc09863fbea1961deea4df60001c732
Igor Gomes <igor.gomes.paradella@gmail.com>
2025-03-24 09:12:51 -0300

    at

--- 3° Semestre/Segunda/Aula_4_metodos_de_ordenacao/Bubblesort.java
+++ 3° Semestre/Segunda/Aula_4_metodos_de_ordenacao/Bubblesort.java
@@ -1,12 +1,24 @@
 public class Bubblesort {
     public static void main(String[] args) {
         int[] vetor = new int[10];
+        int aux;
 
         for(int i = 0; i < vetor.length; i++) {
-            vetor[i] = (int) (Math.random() * 10);
+            vetor[i] = (int) (Math.random() * 100);
+        }
 
+        for(int i = 0; i < vetor.length; i++) {
+            for(int j = i + 1; j < vetor.length; j++) {
+                if(vetor[i] > vetor[j]){
+                    aux = vetor[j];
+                    vetor[j] = vetor[i];
+                    vetor[i] = aux;
+                }
+    
+            }
             System.out.println(vetor[i]);
         }
+        
         
     }
 }
