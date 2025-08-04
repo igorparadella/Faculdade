e84f1724fc7a516a113936cb3c4f7517e29398d4
Igor Gomes <igor.gomes.paradella@gmail.com>
2025-08-04 09:26:29 -0300

    Ultima temporada

--- Server/root/Sexta/28_03_2025/ReservasMaven/src/main/java/model/Acomodacao.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Acomodacao {
-    public String endereco;
-    public double preco_base;
-    public String descricao_acomodacao;
-    public String adicionais;    
-}
